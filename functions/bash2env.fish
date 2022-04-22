c0f6200f622f1f22483d06e69efa3b75323cdd9d
Huang-Huang Bao <eh5@sokka.cn>
2022-04-22 15:18:32 +0800

    improve: add usage

--- functions/bash2env.fish
+++ functions/bash2env.fish
@@ -1,4 +1,12 @@
-function bash2env
+function bash2env -d "Source environment variables modified by given bash command"
+    if test (count $argv) -eq 0
+        echo 'Usage:' \
+            (set_color $fish_color_command)'bash2env' \
+            (set_color $fish_color_param)'<bash command>'
+        return 22
+    end
+
     set -l DIR (dirname (status -f))
     command bash $DIR/__bash2env.sh $argv | source
+    return $status
 end
